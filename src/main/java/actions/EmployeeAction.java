283428ac9f282e4239856ba91480b0c1128d578c
kimuhoan.chan <alisakimhoang@gmail.com>
2022-12-14 22:20:17 +0900

    「従業員editアクションの作成」 Create Employee Edit Action

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -150,5 +150,29 @@ public class EmployeeAction extends ActionBase {
         //詳細画面を表示
         forward(ForwardConst.FW_EMP_SHOW);
     }
+
+/**
+ * 編集画面を表示する
+ * @throws ServletException
+ * @throws IOException
+ */
+public void edit() throws ServletException, IOException {
+
+    //idを条件に従業員データを取得する
+    EmployeeView ev = service.findOne(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+
+    if (ev == null || ev.getDeleteFlag() == AttributeConst.DEL_FLAG_TRUE.getIntegerValue()) {
+
+        //データが取得できなかった、または論理削除されている場合はエラー画面を表示
+        forward(ForwardConst.FW_ERR_UNKNOWN);
+        return;
+    }
+
+    putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
+    putRequestScope(AttributeConst.EMPLOYEE, ev); //取得した従業員情報
+
+    //編集画面を表示する
+    forward(ForwardConst.FW_EMP_EDIT);
+}
 }
 
