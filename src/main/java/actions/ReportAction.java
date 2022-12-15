39701f479a3aa05e2e8e09886e0fba4d8b614963
kimuhoan.chan <alisakimhoang@gmail.com>
2022-12-15 12:53:09 +0900

    「日報showアクションの作成」 Create Daily Report Show Action

--- src/main/java/actions/ReportAction.java
+++ src/main/java/actions/ReportAction.java
@@ -140,4 +140,27 @@ public class ReportAction extends ActionBase {
             }
         }
     }
+
+    /**
+     * 詳細画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void show() throws ServletException, IOException {
+
+        //idを条件に日報データを取得する
+        ReportView rv = service.findOne(toNumber(getRequestParam(AttributeConst.REP_ID)));
+
+        if (rv == null) {
+            //該当の日報データが存在しない場合はエラー画面を表示
+            forward(ForwardConst.FW_ERR_UNKNOWN);
+
+        } else {
+
+            putRequestScope(AttributeConst.REPORT, rv); //取得した日報データ
+
+            //詳細画面を表示
+            forward(ForwardConst.FW_REP_SHOW);
+        }
+    }
 }
